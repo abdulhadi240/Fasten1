e19d2aa2669f69da69f37095cfa9a1810b22e15f
abdulhadi240 <ah912425@gmail.com>
2025-01-17 01:31:12 +0500

    resources

---
+++ public/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./!(build|dist|.*)/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {},
+    },
+    fontSize: {
+      inherit: "inherit",
+    },
+    screens: {
+      mq1225: {
+        raw: "screen and (max-width: 1225px)",
+      },
+      mq1050: {
+        raw: "screen and (max-width: 1050px)",
+      },
+      mq750: {
+        raw: "screen and (max-width: 750px)",
+      },
+      mq450: {
+        raw: "screen and (max-width: 450px)",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
